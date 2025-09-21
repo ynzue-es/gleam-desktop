8e120e1304cb3908c9b01787566f1535a7945d73
yannis <yannis.nzuepro@gmail.com>
2025-09-21 12:11:53 +0200

    fiw infinite loop

--- electron/main.cjs
+++ electron/main.cjs
@@ -1,24 +1,7 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain, shell } = require("electron");
 const path = require("path");
 
 function createWindow() {
-  app.whenReady().then(() => {
-    createWindow()
-    app.setAsDefaultProtocolClient("gleam")
-
-    ipcMain.handle("open-external", async (event, url) => {
-      await shell.openExternal(url)
-    })
-  })
-
-  app.on("open-url", (event, url) => {
-    event.preventDefault()
-    if (url.startsWith("gleam://auth/callback")) {
-      const win = BrowserWindow.getAllWindows()[0]
-      win.webContents.send("auth-callback", url)
-    }
-  })
-
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
@@ -27,14 +10,25 @@ function createWindow() {
     },
   });
 
-
-
   if (process.env.NODE_ENV === "development") {
     win.loadURL("http://localhost:5173");
     win.webContents.openDevTools();
   } else {
     win.loadFile(path.join(__dirname, "../dist/index.html"));
   }
+
+  app.setAsDefaultProtocolClient("gleam");
+
+  ipcMain.handle("open-external", async (event, url) => {
+    await shell.openExternal(url);
+  });
+
+  app.on("open-url", (event, url) => {
+    event.preventDefault();
+    if (url.startsWith("gleam://auth/callback")) {
+      win.webContents.send("auth-callback", url);
+    }
+  });
 }
 
 app.whenReady().then(createWindow);
